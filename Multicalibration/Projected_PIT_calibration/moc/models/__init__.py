9a2eb2996d1bd31c9a61b42c338440386d386c03
Elnura Zhalieva <elnura.zhalieva.7@gmail.com>
2025-04-17 15:18:18 +0400

    pce per dimensions for all datasets

--- Multicalibration/Projected_PIT_calibration/moc/models/__init__.py
+++ Multicalibration/Projected_PIT_calibration/moc/models/__init__.py
@@ -2,27 +2,30 @@ from moc.models.trainers.default_trainer import DefaultTrainer
 from moc.models.trainers.lightning_trainer import get_lightning_trainer
 
 from moc.models.oracle.oracle_model import OracleModel
-from moc.models.drf_kde.drf_kde import DRF_KDE
+# from moc.models.drf_kde.drf_kde import DRF_KDE
 from moc.models.mqf2.lightning_module import MQF2LightningModule
-from moc.models.glow.glow import GlowPreTrained
+# from moc.models.glow.glow import GlowPreTrained
 from moc.models.mixture.mixture_model import MixtureLightningModule
 from moc.models.quantile.quantile_model import QuantileModule
+from moc.models.gaussian.gaussian import GaussianLightningModule
 
 
 models = {
     'Oracle': OracleModel,
-    'DRF-KDE': DRF_KDE,
+    # 'DRF-KDE': DRF_KDE,
     'MQF2': MQF2LightningModule,
     'Mixture': MixtureLightningModule,
-    'Glow': GlowPreTrained,
+    # 'Glow': GlowPreTrained,
     'Quantile': QuantileModule,
+    'Gaussian': GaussianLightningModule,
 }
 
 trainers = {
     'Oracle': DefaultTrainer,
-    'DRF-KDE': DefaultTrainer,
+    # 'DRF-KDE': DefaultTrainer,
     'MQF2': get_lightning_trainer,
     'Mixture': get_lightning_trainer,
-    'Glow': DefaultTrainer,
+    # 'Glow': DefaultTrainer,
     'Quantile': get_lightning_trainer,
+    'Gaussian': get_lightning_trainer,
 }
