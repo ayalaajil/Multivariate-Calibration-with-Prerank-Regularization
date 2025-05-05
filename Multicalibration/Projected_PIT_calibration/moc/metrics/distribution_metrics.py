aa505368560a0175f2a82fca5838e5318f1b173b
ryuzakizh <elnura.zhalieva.7@gmail.com>
2025-05-05 10:25:20 +0000

    reliability plots

--- Multicalibration/Projected_PIT_calibration/moc/metrics/distribution_metrics.py
+++ Multicalibration/Projected_PIT_calibration/moc/metrics/distribution_metrics.py
@@ -75,7 +75,7 @@ def calculate_PIT(dist, y, n_samples, setup, prerank):
         print("samples.shape")
         print(samples.shape)
     else: 
-        samples = dist.sample((n_samples,)).permute(1, 0, 2) #256,100,4
+        samples = dist.sample((n_samples,)).permute(1, 0, 2) #256,20,4
     pits = []
     explained_var = np.ones(dim) * (1/dim)
     if prerank in ['mean', 'variance', 'dependency']:
@@ -140,35 +140,39 @@ def pce(dist, y, n_samples = 1000, mode = 'all', prerank = 'pca', setup = 'real'
     alphas = torch.linspace(0, 1, 100, device=y.device)
     pit_values, _ = calculate_PIT(dist, y, n_samples = n_samples, setup = setup, prerank = prerank) #shape (4,256,1) or (1, 256,1)
     dim = pit_values.shape[0]
-    pces = []
+    pces, cdfs = [], []
     for d in range(dim):
         pits = pit_values[d].view(-1)  # shape: (256,)
         pits_sorted = pits.sort()[0]
         cdf_estimates = torch.searchsorted(pits_sorted, alphas, side='right') / pits_sorted.numel()
         pce = torch.mean(torch.abs(cdf_estimates - alphas))
         pces.append(pce)
-    pces = torch.stack(pces)
-    explained_var = torch.from_numpy(_).to(pces.device)  # shape: (4,)
-    if mode == 'average':
-        if prerank == 'pca':
-            return (pces * explained_var).sum()
-        return pces.mean()
-    elif mode == 'all':
-        return pces #it returns a list with 4 values, pce corresponding to each PCA
-
-def reliability_plots(dist, y, n_samples = 1000, mode = 'all', prerank = 'pca', setup = 'real'):
-    alphas = torch.linspace(0, 1, 100, device=y.device)
-    pit_values, _ = calculate_PIT(dist, y, n_samples = n_samples, setup = setup, prerank = prerank) #shape (4,256,1) or (1, 256,1)
-    dim = pit_values.shape[0]
-    cdfs = []
-    for d in range(dim):
-        pits = pit_values[d].view(-1)  # shape: (256,)
-        pits_sorted = pits.sort()[0]
-        cdf_estimates = torch.searchsorted(pits_sorted, alphas, side='right') / pits_sorted.numel()
         cdfs.append(cdf_estimates)
+    pces = torch.stack(pces)
     cdfs = torch.stack(cdfs)
-    explained_var = torch.from_numpy(_).to(cdfs.device).unsqueeze(-1)  # shape: (4,)
-    if prerank =='pca':
-        return (cdfs * explained_var).sum(dim=0)
-    return cdfs.mean(dim=0)
+    # explained_var = torch.from_numpy(_).to(cdfs.device)  # shape: (4,)
+    explained_var = _
+    return pces, cdfs, explained_var
+    # if mode == 'average':
+    #     if prerank == 'pca':
+    #         return (pces * explained_var).sum()
+    #     return pces.mean()
+    # elif mode == 'all':
+    #     return pces #it returns a list with 4 values, pce corresponding to each PCA
+
+# def reliability_plots(dist, y, n_samples = 1000, mode = 'all', prerank = 'pca', setup = 'real'):
+#     alphas = torch.linspace(0, 1, 100, device=y.device)
+#     pit_values, _ = calculate_PIT(dist, y, n_samples = n_samples, setup = setup, prerank = prerank) #shape (4,256,1) or (1, 256,1)
+#     dim = pit_values.shape[0]
+#     cdfs = []
+#     for d in range(dim):
+#         pits = pit_values[d].view(-1)  # shape: (256,)
+#         pits_sorted = pits.sort()[0]
+#         cdf_estimates = torch.searchsorted(pits_sorted, alphas, side='right') / pits_sorted.numel()
+#         cdfs.append(cdf_estimates)
+#     cdfs = torch.stack(cdfs)
+#     explained_var = torch.from_numpy(_).to(cdfs.device).unsqueeze(-1)  # shape: (4,)
+#     if prerank =='pca':
+#         return (cdfs * explained_var).sum(dim=0)
+#     return cdfs.mean(dim=0)
 
